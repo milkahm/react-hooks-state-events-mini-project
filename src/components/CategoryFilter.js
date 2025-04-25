6197b7bca865bc6bc907a878419ca3c69107cf8c
milly <milkah.maina@student.moringaschool.com>
2025-04-25 20:36:25 +0300

    initial

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,32 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({categories, setFilter}) {
+
+  function handleClick(event){
+    setFilter(this)
+    clearSelection()
+    event.target.classList.add("selected")
+  }
+
+  function clearSelection(){
+    if (document.querySelector(".selected"))
+    {
+      document.querySelector(".selected")
+      .classList.remove("selected")
+    }
+      
+  }
   return (
     <div className="categories">
       <h5>Category filters</h5>
       {/* render <button> elements for each category here */}
+      {
+        categories.map(category=>{
+          return <button className="" key={category} onClick={handleClick.bind(category)} >
+            {category}
+          </button>
+        })
+      }
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,24 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({tasks, handleDelete}) {
+ 
+  let id = 0;
   return (
     <div className="tasks">
       {/* display a list of tasks using Task component */}
+      {
+        tasks.map((task =>{
+          id += 1
+          task.id = id;
+          return <Task 
+          key={task.id} 
+          text={task.text} 
+          category={task.category} 
+          onClickDelete={()=>handleDelete(task.id)}/>
+        }))
+        
+      }
     </div>
   );
 }
